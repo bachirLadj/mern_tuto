c0f3a1d4e2d1c656459b9cc19ed1b4fa872e5ebc
bachirLadj <fb_ladjouzi@esi.dz>
2020-02-07 12:10:06 +0100

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://bachirladj:mouloudia1921@cluster0-shard-00-00-mzboe.gcp.mongodb.net:27017,cluster0-shard-00-01-mzboe.gcp.mongodb.net:27017,cluster0-shard-00-02-mzboe.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+}    
